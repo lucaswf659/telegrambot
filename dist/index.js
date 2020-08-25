e41c4adac42ce8d90471bc81b24e96d02225503a
Lucas Weber <Lucaswf659@gmail.com>
2020-08-25 13:54:51 -0300

    chore: starting configuration - added eslint, nodemon, prettier and sucrase

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";//import { messages } from './messages/messages'
+
+// const Telegraf = require('telegraf')
+// const axios = require('axios')
+// const bot = new Telegraf('1350766014:AAHXa5KPBnJZLHqr14tJPm9wWcMEs6H8XiE')
+
+
+// bot.use((ctx) =>{
+//     ctx.reply('Hello!!')
+// })
+
+// bot.start((ctx) => {
+//     ctx.reply(messages.welcome_message)
+// }) 
+
+// bot.command('fortune', (ctx) =>{
+//     url = "http://yerkee.com/api/fortune"
+
+//     axios.get(url)
+//     .then((res)=>{
+//         ctx.reply(res.data.fortune)
+//     })
+// })
+
+//bot.launch()
